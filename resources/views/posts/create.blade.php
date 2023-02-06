556c6f956a42c98b0068f1b416674c89005274be
kawabetomoki <ec2-user@ip-172-31-18-220.ec2.internal>
2023-02-06 14:15:56 +0900

    add 記事投稿作成画面

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+       <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<!--         <link href="https://app.diagrams.net/#G1Mi5nSzK-3830EMWIX6IAJ4038p6EjzrZ">-->
+    
+    </head>
+    <body class="antialiased">
+        <h1>Blog Name</h1>
+        <from action="/posts" method="POST">
+            @csrf
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name=post[title] placeholder="タイトル"/>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も一日お疲れ様でした。"></textarea>
+            </div>
+            <input type="submit" value="store">
+        </from>
+        <div class='footer'>
+            <a href="/">戻る</a>
+        </div>
+    </body>
+</html>
